df95af9edb94fe62c1ddef9f3b890c50c3129cb9
noemidasta-03 <noemi.dasta18@gmail.com>
2024-12-23 18:45:52 +0100

    create navbar with bootstrapReact

---
+++ src/components/CustomNavbar.tsx
@@ -0,0 +1,37 @@
+import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./CustomNavbar.css";
+
+const CustomNavbar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary  container-navbar">
+      <Container>
+        <Navbar.Brand href="#home" className="brand-navbar">
+          Pedagogista
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
